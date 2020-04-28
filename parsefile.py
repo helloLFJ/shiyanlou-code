202288522628f4e5c237dc15cbc7d7e51e29d484
helloLFJ <lifengjiao@genomics.cn>
2020-04-28 14:23:55 +0800

    文件学习

---
+++ parsefile.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os
+import sys
+def parse_file(path):
+    """
+    分析给定的文本文件，返回其空格，制表符，行相关信息
+    ：arg path:要分析文本文件的路径
+    ：return：包含空格数，制表格数，行数的元组
+    """
+    fd = open(path)
+    i = 0
+    spaces = 0
+    tabs = 0
+    for i,line in enumerate(fd):
+        spaces += line.count(" ")
+        tabs += line.count("\t")
+#现在关闭打开的文件
+    fd.close()
+#以元组的形式返回结果
+    return spaces, tabs, i+1
+def main(path):
+    """
+    函数用于打印文件分析结果
+    ：arg path:要分析的文本文件的路径
+    ：return：若文件存在则为True，否则为 False
+    """
+    if os.path.exists(path):
+        spaces, tabs, lines = parse_file(path)
+        print("spaces {}. tabs {}. lines {}".format(spaces, tabs, lines))
+        return True
+    else:
+        return False
+if __name__ == "__main__":
+    if len(sys.argv)>1:
+        main(sys.argv[1])
+    else:
+        sys.exit(-1)
+    sys.exit(0)
+# 调用方法： ./parefile.py 文件路径
